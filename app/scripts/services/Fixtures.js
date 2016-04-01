c1df615b8be7d853af785b16f8e45425dddd0d4b
Nate <npauzenga@gmail.com>
2016-04-01 19:38:34 -0400

    complete checkpoint 6

---
+++ app/scripts/services/Fixtures.js
@@ -0,0 +1,45 @@
+(function() {
+  function Fixtures() {
+    var Fixtures = {};
+
+    var albumAllHailWestTexas = {
+      title: "All Hail West Texas",
+      artist: "The Mountain Goats",
+      label: "Rough Trade",
+      year: "2001",
+      albumArtUrl: "/assets/images/album_covers/01.png",
+      songs: [
+      { title: "Blue", duration: 161.71, audioUrl: "/assets/music/blue" },
+      { title: "Green", duration: 103.96, audioUrl: "/assets/music/green" },
+      { title: "Red", duration: 268.45, audioUrl: "/assets/music/red" },
+      { title: "Pink", duration: 153.14, audioUrl: "/assets/music/pink" },
+      { title: "Magenta", duration: 374.22, audioUrl: "/assets/music/magenta" }
+      ]
+    };
+
+    var albumLifeAndDeath = {
+      title: "Life and Death of an American Fourtracker",
+      artist: "John Vanderslice",
+      label: "Barsuk",
+      year: "2003",
+      albumArtUrl: "/assets/images/album_covers/01.png",
+      songs: [
+      { title: "Blue", duration: "4:26" },
+      { title: "Green", duration: "3:26" },
+      { title: "Red", duration: "4:16" },
+      { title: "Pink", duration: "4:24" },
+      { title: "Magenta", duration: "6:26" }
+      ]
+    };
+
+    Fixtures.getAlbum = function() {
+      return albumAllHailWestTexas;
+    };
+
+    return Fixtures;
+  }
+
+  angular
+    .module('blocJams')
+    .factory('Fixtures', Fixtures);
+})();
